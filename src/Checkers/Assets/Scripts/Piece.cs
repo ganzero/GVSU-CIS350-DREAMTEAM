a2a4785d91d63ce39e611850e01d39a51ce16556
Jesse Leonard <leonajes@mail.gvsu.edu>
2021-04-15 18:35:47 -0400

    King piece moves backwards

--- src/Checkers/Assets/Scripts/Piece.cs
+++ src/Checkers/Assets/Scripts/Piece.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class Piece : MonoBehaviour
 {
     public bool isWhite;
-    //public bool isKing;
+    public bool isKing;
 
-    public bool isKing(int x1, int y1)
+    public void isAKing(int x1, int y1)
     {
-        if (isWhite && ((x1 <= 8) && (y1 == 7)) )
+        if (isWhite && ((x1 <= 8) && (y1 == 7))) {
             //board has to be empty and white piece has to be at other end
-            return true;
-        return false;
-
-
-        if (isWhite && ((x1 <= 8) && (y1 == 0)))
+            isKing = true;
+        }
+        else if (!isWhite && ((x1 <= 8) && (y1 == 0)))
+        {
             //place has to be empty and black piece has to be at other end
-            return true;
-        return false;
+            isKing = true;
+        }
     }
 
     public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2)
@@ -30,7 +29,8 @@ public class Piece : MonoBehaviour
         int moveX = Mathf.Abs(x1 - x2);
         int moveY = y2 - y1;
         // Normal Move
-        if (isWhite || isKing(x2, y2))
+        isAKing(x2, y2);
+        if (isWhite || isKing)
         {
             if (moveX == 1)
             {
@@ -51,7 +51,7 @@ public class Piece : MonoBehaviour
         }
 
         // Black checks different on Y axis
-        if (!isWhite || isKing(x2, y2))
+        if (!isWhite || isKing)
         {
             if (moveX == 1)
             {
